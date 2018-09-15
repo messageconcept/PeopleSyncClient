cca89f9b060d57ba0cecc7dad9179ea9b4d9b121
Michael Biebl <mbiebl@messageconcept.com>
2018-09-15 17:06:45 +0200

    Remove donation/open source intro fragment

--- app/src/main/java/com/messageconcept/peoplesyncclient/ui/AppSettingsActivity.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/ui/AppSettingsActivity.kt
@@ -21,7 +21,6 @@ import com.messageconcept.peoplesyncclient.BuildConfig
 import com.messageconcept.peoplesyncclient.R
 import com.messageconcept.peoplesyncclient.settings.Settings
 import com.messageconcept.peoplesyncclient.ui.intro.BatteryOptimizationsFragment
-import com.messageconcept.peoplesyncclient.ui.intro.OpenSourceFragment
 import com.messageconcept.peoplesyncclient.ui.intro.OpenTasksFragment
 import com.google.android.material.snackbar.Snackbar
 import java.net.URI
@@ -146,7 +145,6 @@ class AppSettingsActivity: AppCompatActivity() {
             settings.remove(BatteryOptimizationsFragment.Model.HINT_BATTERY_OPTIMIZATIONS)
             settings.remove(BatteryOptimizationsFragment.Model.HINT_AUTOSTART_PERMISSION)
             settings.remove(OpenTasksFragment.Model.HINT_OPENTASKS_NOT_INSTALLED)
-            settings.remove(OpenSourceFragment.Model.SETTING_NEXT_DONATION_POPUP)
             Snackbar.make(requireView(), R.string.app_settings_reset_hints_success, Snackbar.LENGTH_LONG).show()
         }
 
