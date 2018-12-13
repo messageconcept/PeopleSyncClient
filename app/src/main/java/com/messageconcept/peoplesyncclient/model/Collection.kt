ff6729da4f6cfaa74db951537843cc9d3901a7f0
Michael Biebl <mbiebl@messageconcept.com>
2018-12-13 16:33:52 +0100

    Automatically select newly found collections
    We want collections to be subscribed by default while still providing
    the ability to deselect collection if the user so chooses.

--- app/src/main/java/com/messageconcept/peoplesyncclient/model/Collection.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/model/Collection.kt
@@ -51,7 +51,7 @@ data class Collection(
     var source: HttpUrl? = null,
 
     /** whether this collection has been selected for synchronization */
-    var sync: Boolean = false
+    var sync: Boolean = true
 
 ): IdEntity() {
 
