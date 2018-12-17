e2fa81ce841078f108f70ba604a3117ea2d70848
Michael Biebl <mbiebl@messageconcept.com>
2018-12-17 16:02:54 +0100

    Trigger a sync request when the number of collection changes
    The collection detection via DavService happens asynchronously in
    AddressBooksSyncAdapterService. If a new collection is found during the
    discovery process, the new address book would only be synced after yet
    another sync schedule (the default is 4h).
    To speed this up, trigger a manual sync request from DavService if the
    number of collections changes.

--- app/src/main/java/com/messageconcept/peoplesyncclient/DavService.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/DavService.kt
@@ -294,6 +294,8 @@ class DavService: android.app.Service() {
                     else
                         deselectedCollections += url
                 }
+                // remember number of collections before the refresh
+                val numCollectionsOld = collections.size
 
                 // now refresh homesets and their member collections
                 val itHomeSets = homeSets.iterator()
@@ -363,6 +365,14 @@ class DavService: android.app.Service() {
                     collections[url]?.let { it.sync = true }
                 for (url in deselectedCollections)
                     collections[url]?.let { it.sync = false }
+
+                val numCollectionsNew = collections.size;
+                if (numCollectionsOld != numCollectionsNew) {
+                    Logger.log.info("Number of collections changed for ${account.name} from ${numCollectionsOld} -> ${numCollectionsNew}, triggering sync")
+                    val args = Bundle(1)
+                    args.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true)
+                    ContentResolver.requestSync(account, getString(R.string.address_books_authority), args)
+                }
             }
 
             saveResults()
