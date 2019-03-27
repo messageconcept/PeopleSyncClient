88880459407998eb03cd9d2a08517f1c404f8b99
Michael Biebl <mbiebl@messageconcept.com>
2019-03-27 13:02:46 +0100

    Add action button to upgrade notification
    While the Android design guidelines recommend to not use the same intent
    for action buttons and the tap action, having a separate action button
    makes the intention clearer and hopefully encourages the user to start
    the app and finish the upgrade.

--- app/src/main/java/com/messageconcept/peoplesyncclient/ui/AccountsActivity.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/ui/AccountsActivity.kt
@@ -16,6 +16,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.GravityCompat
 import com.messageconcept.peoplesyncclient.R
 import com.messageconcept.peoplesyncclient.settings.Settings
@@ -66,6 +67,8 @@ class AccountsActivity: AppCompatActivity(), NavigationView.OnNavigationItemSele
 
         nav_view.setNavigationItemSelectedListener(this)
         nav_view.itemIconTintList = null
+
+        NotificationManagerCompat.from(this).cancel(NotificationUtils.NOTIFY_UPDATE)
     }
 
     override fun onResume() {
