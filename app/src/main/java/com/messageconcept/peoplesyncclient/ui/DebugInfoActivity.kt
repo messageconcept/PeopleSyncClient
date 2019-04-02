285551b3677fadb2bbbbadbdc87aaa5c366b9651
Michael Biebl <mbiebl@messageconcept.com>
2019-04-02 02:13:42 +0200

    Fix debug info report file name

--- app/src/main/java/com/messageconcept/peoplesyncclient/ui/DebugInfoActivity.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/ui/DebugInfoActivity.kt
@@ -488,7 +488,7 @@ class DebugInfoActivity: AppCompatActivity() {
                 try {
                     zipProgress.postValue(true)
 
-                    val zipFile = File(debugInfoDir, "davx5-debug.zip")
+                    val zipFile = File(debugInfoDir, "peoplesync-debug.zip")
                     Logger.log.fine("Writing debug info to ${zipFile.absolutePath}")
                     ZipOutputStream(zipFile.outputStream().buffered()).use { zip ->
                         zip.setLevel(9)
