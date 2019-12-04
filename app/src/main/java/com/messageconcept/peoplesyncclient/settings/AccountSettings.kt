dfe2cd2fae90d1d4b3a25d64e468e7c7e416a2b2
Michael Biebl <mbiebl@messageconcept.com>
2019-12-04 20:08:21 +0100

    Store baseUrl in userData for managed accounts
    This will make it easier to identify accounts that were created from
    managed configuration data.

--- app/src/main/java/com/messageconcept/peoplesyncclient/settings/AccountSettings.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/settings/AccountSettings.kt
@@ -54,6 +54,7 @@ class AccountSettings(
 
         const val KEY_USERNAME = "user_name"
         const val KEY_CERTIFICATE_ALIAS = "certificate_alias"
+        const val KEY_BASE_URL = "base_url"
 
         const val KEY_WIFI_ONLY = "wifi_only"               // sync on WiFi only (default: false)
         const val KEY_WIFI_ONLY_SSIDS = "wifi_only_ssids"   // restrict sync to specific WiFi SSIDs
@@ -76,6 +77,9 @@ class AccountSettings(
                 Credentials.Type.ClientCertificate ->
                     bundle.putString(KEY_CERTIFICATE_ALIAS, credentials.certificateAlias)
             }
+
+            if (credentials.baseUrl != null)
+                bundle.putString(KEY_BASE_URL, credentials.baseUrl)
 
             return bundle
         }
