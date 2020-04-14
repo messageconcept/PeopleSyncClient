72f0564d23508f7f7ac3d81de89d07ab7eed7408
Alex Baker <alex@tasks.org>
2020-04-14 10:22:17 -0500

    Clear url, username, and name errors
    https://gitlab.com/bitfireAT/davx5-ose/-/merge_requests/30

--- app/src/main/java/com/messageconcept/peoplesyncclient/ui/setup/DefaultLoginCredentialsModel.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/ui/setup/DefaultLoginCredentialsModel.kt
@@ -1,6 +1,7 @@
 package com.messageconcept.peoplesyncclient.ui.setup
 
 import android.content.Intent
+import android.text.Editable
 import androidx.annotation.MainThread
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -36,7 +37,17 @@ class DefaultLoginCredentialsModel: ViewModel() {
         loginPasswordManaged.value = false
     }
 
-    fun clearPasswordError(s: CharSequence, start: Int, before: Int, count: Int) {
+    fun clearUrlError(s: Editable) {
+        if (s.toString() != "https://") {
+            baseUrlError.value = null
+        }
+    }
+
+    fun clearUsernameError(s: Editable) {
+        usernameError.value = null
+    }
+
+    fun clearPasswordError(s: Editable) {
         passwordError.value = null
     }
 
