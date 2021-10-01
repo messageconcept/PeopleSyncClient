9ff2f870e0ffb2adf6d8c237c20b11a2e8b82a32
Michael Biebl <mbiebl@messageconcept.com>
2021-10-01 19:32:43 +0200

    Make sure we do not accidentally log a password

--- app/src/main/java/com/messageconcept/peoplesyncclient/settings/SettingsManager.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/settings/SettingsManager.kt
@@ -90,9 +90,10 @@ class SettingsManager private constructor(
         for (provider in providers)
             try {
                 val value = reader(provider)
-                Logger.log.finer("${provider::class.java.simpleName}: $key = $value")
+                val maskedValue = SettingsUtils.filterPassword(key, value)
+                Logger.log.finer("${provider::class.java.simpleName}: $key = $maskedValue")
                 if (value != null) {
-                    Logger.log.fine("Looked up setting $key -> $value")
+                    Logger.log.fine("Looked up setting $key -> $maskedValue")
                     return value
                 }
             } catch(e: Exception) {
