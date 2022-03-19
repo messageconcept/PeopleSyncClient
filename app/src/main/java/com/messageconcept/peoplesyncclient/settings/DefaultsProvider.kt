a68c38c7a6b0c46b502944bd6c02ffc7b61fd75f
Michael Biebl <mbiebl@messageconcept.com>
2022-03-20 00:42:49 +0100

    Sync new collections by default

--- app/src/main/java/com/messageconcept/peoplesyncclient/settings/DefaultsProvider.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/settings/DefaultsProvider.kt
@@ -19,7 +19,7 @@ class DefaultsProvider(
 
     override val booleanDefaults = mutableMapOf(
         Pair(Settings.DISTRUST_SYSTEM_CERTIFICATES, false),
-        Pair(Settings.SYNC_ALL_COLLECTIONS, false)
+        Pair(Settings.SYNC_ALL_COLLECTIONS, true)
     )
 
     override val intDefaults = mapOf(
