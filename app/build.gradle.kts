db291ad70d391a58217af1c6efdfd76492859ed1
Michael Biebl <mbiebl@messageconcept.com>
2024-08-09 14:51:25 +0200

    Update version information for 4.3-13 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.messageconcept.peoplesyncclient"
 
         //noinspection HighAppVersionCode
-        versionCode = 2070040312
-        versionName = "4.3-12"
+        versionCode = 2070040313
+        versionName = "4.3-13"
 
         buildConfigField("long", "buildTime", "${System.currentTimeMillis()}L")
 
