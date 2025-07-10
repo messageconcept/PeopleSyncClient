c97158c0a48a838fa938d9b16e8fdb72fd0f9c5b
Michael Biebl <mbiebl@messageconcept.com>
2025-07-10 15:51:17 +0200

    Revert "Use alternative approach for dealing with edge-to-edge issues"
    This reverts commit af021e13801074fc4fac937320c50cbb6d168304.

--- app/src/main/kotlin/com/messageconcept/peoplesyncclient/ui/AppTheme.kt
+++ app/src/main/kotlin/com/messageconcept/peoplesyncclient/ui/AppTheme.kt
@@ -5,19 +5,19 @@
 package com.messageconcept.peoplesyncclient.ui
 
 import androidx.activity.SystemBarStyle
+import androidx.activity.compose.LocalActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.displayCutoutPadding
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
@@ -28,13 +28,14 @@ import com.messageconcept.peoplesyncclient.ui.composable.SafeAndroidUriHandler
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    windowInsets: WindowInsets = WindowInsets.safeDrawing,
     content: @Composable () -> Unit
 ) {
-    val view = LocalView.current
+    val activity = LocalActivity.current
     SideEffect {
         // If applicable, call Activity.enableEdgeToEdge to enable edge-to-edge layout on Android <15, too.
         // When we have moved everything into one Activity with Compose navigation, we can call it there instead.
-        (view.context as? AppCompatActivity)?.enableEdgeToEdge(
+        (activity as? AppCompatActivity)?.enableEdgeToEdge(
             navigationBarStyle = SystemBarStyle.auto(
                 lightScrim = M3ColorScheme.lightScheme.scrim.toArgb(),
                 darkScrim = M3ColorScheme.darkScheme.scrim.toArgb()
@@ -51,13 +52,14 @@ fun AppTheme(
             else
                 M3ColorScheme.darkScheme,
         ) {
-            Box(Modifier.fillMaxSize().displayCutoutPadding().navigationBarsPadding().clipToBounds()) {
+            Box(Modifier.windowInsetsPadding(windowInsets)) {
                 content()
             }
         }
     }
     
     // Track if the app is in the foreground
+    val view = LocalView.current
     LifecycleResumeEffect(view) {
         ForegroundTracker.onResume()
         onPauseOrDispose {
