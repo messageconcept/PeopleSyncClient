4e8e03d097f20c80821a4fff26b6412ab6c10f6a
Michael Biebl <mbiebl@messageconcept.com>
2025-09-15 15:44:30 +0200

    Fix debug info report file name

--- app/src/main/kotlin/com/messageconcept/peoplesyncclient/ui/DebugInfoModel.kt
+++ app/src/main/kotlin/com/messageconcept/peoplesyncclient/ui/DebugInfoModel.kt
@@ -148,7 +148,7 @@ class DebugInfoModel @AssistedInject constructor(
         try {
             uiState = uiState.copy(zipInProgress = true)
 
-            val file = File(LogFileHandler.debugDir(context), "davx5-debug.zip")
+            val file = File(LogFileHandler.debugDir(context), "peoplesync-debug.zip")
             logger.fine("Writing debug info to ${file.absolutePath}")
             ZipOutputStream(file.outputStream().buffered()).use { zip ->
                 zip.setLevel(9)
