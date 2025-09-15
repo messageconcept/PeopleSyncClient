dd80cb21f4ff9f3f5968d9a773bd98345e921e11
Michael Biebl <mbiebl@messageconcept.com>
2025-09-15 15:44:29 +0200

    Remove Nextcloud LoginType
    We don't support Nextcloud's Login Flow in PeopleSync.
    
    This also means, we don't need to detect browsers that support Custom
    Tabs, so we can drop the corresponding intent-filter, as it is only
    required for Nextcloud Login Flow.
    
    See also https://developers.google.com/web/updates/2020/07/custom-tabs-android-11

--- app/src/ose/kotlin/com/messageconcept/peoplesyncclient/ui/setup/StandardLoginTypesProvider.kt
+++ app/src/ose/kotlin/com/messageconcept/peoplesyncclient/ui/setup/StandardLoginTypesProvider.kt
@@ -24,7 +24,6 @@ class StandardLoginTypesProvider @Inject constructor(
         val specificLoginTypes = listOf(
             FastmailLogin,
             GoogleLogin,
-            NextcloudLogin
         )
     }
 
@@ -33,8 +32,6 @@ class StandardLoginTypesProvider @Inject constructor(
     override fun intentToInitialLoginType(intent: Intent): LoginAction =
         intent.data?.normalizeScheme().let { uri ->
             when {
-                intent.hasExtra(LoginActivity.EXTRA_LOGIN_FLOW) ->
-                    LoginAction(NextcloudLogin, true)
                 uri?.scheme == "mailto" ->
                     LoginAction(EmailLogin, true)
                 listOf("carddavs", "http", "https").any { uri?.scheme == it } ->
