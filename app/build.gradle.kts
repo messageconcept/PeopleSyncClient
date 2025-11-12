7603c398accc5237d58bdff063452158e96ffd99
Michael Biebl <mbiebl@messageconcept.com>
2025-11-12 19:17:09 +0100

    Update version information for 4.5-1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.messageconcept.peoplesyncclient"
 
         //noinspection HighAppVersionCode
-        versionCode = 2070040500
-        versionName = "4.5-0"
+        versionCode = 2070040501
+        versionName = "4.5-1"
 
         base.archivesName = "PeopleSyncClient-$versionName"
 
